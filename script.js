07e7523194585ba87083c504285d8652963a119f
Yasota2 <101420438+Yasota2@users.noreply.github.com>
2022-03-17 00:12:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,133 +1,44 @@
+'use strict';
 
 
-class Calculator  {
-    constructor(secondOperandTextElement,curentOperandTextElement){
-        this.secondOperandTextElement = secondOperandTextElement;
-        this.curentOperandTextElement = curentOperandTextElement;
-        this.clear();
+const outputTextElement = document.querySelector("[data-output]");
+const generateButton = document.querySelector("[data-generate-pass]");
+let numericArray = []
+let password = "" ;
+
+
+function generatePassword(){
+
+    for ( let i = 0 ; i <= 18-1 ; i++){
+
+        numericArray[i] = Math.floor( Math.random() * (127-32) +32 );
+        password = password + String.fromCharCode(numericArray[i]);
     }
-
-    clear(){
-        this.currentOperand = "",
-        this.secondOperand = "",
-        this.operation = undefined;
-    }
-
-    delete(){
-        this.currentOperand = this.currentOperand.toString().slice(0,-1);
-
-    }
-
-    appendNumber(number){
-        if (number === "." && this.currentOperand.includes('.')) return;
-        if (number === "0" && this.currentOperand.includes('0')) return;
-        this.currentOperand = this.currentOperand.toString() + number.toString();
-        
-        
-    }
-
-    choiseOperation(operation){
-        if (this.currentOperand === "") return;
-        if (this.secondOperand !== ''){
-            this.compute();
-        }
-
-        this.operation = operation;
-        this.secondOperand = this.currentOperand.toString() + " " + this.operation;
-        this.currentOperand = "";
-        
-    }
-
-    compute(){
-        let result ;
-        const second = parseFloat(this.secondOperand);
-        const current = parseFloat(this.currentOperand);
-        if (isNaN(second) || isNaN(current)) return;
-
-        switch (this.operation.toString()){
-
-            case '+' : 
-                result = second+current;
-            break;
-            
-            case '-' : 
-                result = second-current;
-            break;
-
-            case '*' : 
-                result = second*current;
-            break;
-
-            case '÷' :
-                result = second/current;
-
-            default :
-                return;
-        }
-
-        this.currentOperand = result;
-        this.operation = undefined;
-        this.secondOperand = ''
-
-    }
-
-    updateDisplay(){
-        // if (this.operation != null){
-        //     this.secondOperandTextElement.innerText = `${this.secondOperand} ${this.operation.toString()}`;
-        // }
-        
-        this.curentOperandTextElement.innerText = this.currentOperand;
-        this.secondOperandTextElement.innerText = this.secondOperand;
-        
-    }
+    return password;
 }
 
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operations]');
-const equalsButton = document.querySelector('[data-equals]');
-const deleteButton = document.querySelector('[data-delete]');
-const allClearButton = document.querySelector('[data-all-clear]');
-const secondOperandTextElement = document.querySelector('[data-second-operand]');
-const curentOperandTextElement = document.querySelector('[data-curent-operand]');
+
+function updateDisplay(){
+    outputTextElement.innerHTML = password;
+}
+
+
+function clear(){
+    password = ""
+    outputTextElement.innerHTML = ' ';
+}
 
 
 
-const calculator = new Calculator(secondOperandTextElement,curentOperandTextElement);
+generateButton.addEventListener('click', button =>{
 
-numberButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        
-        calculator.appendNumber(button.innerText);
-        calculator.updateDisplay();
-    })
+    if (outputTextElement.innerHTML != " "){
+        clear();
+    }
+    generatePassword();
+    updateDisplay()
+
+
 })
 
 
-operationButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        calculator.choiseOperation(button.innerText);
-        console.log(calculator.currentOperand);
-        calculator.updateDisplay();
-    })
-})
-
-equalsButton.addEventListener('click', button =>{
-
-    calculator.compute();
-    calculator.updateDisplay();
-
-})
-
-allClearButton.addEventListener('click', button =>{
-
-    calculator.clear();
-    calculator.updateDisplay();
-
-})
-
-deleteButton.addEventListener('click', button =>{
-
-    calculator.delete();
-    calculator.updateDisplay();
-
-})
